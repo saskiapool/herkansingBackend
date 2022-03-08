f8f5e94171b5ebc5650c7bf6cb89c377714412d4
Saskia Pool <poolsaskia@gmail.com>
2022-03-08 12:13:28 +0100

    peer review push

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-const express = require('express')
+const express = require('express');
+const { partialRight } = require('lodash');
 const app = express()
-const port = 3000
+const port = 8080
+const path = require('path')
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
+app.get('/', function(_req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.get('/', (req, res) => {
+//     res.send('Hello world')
+// })
+
+// app.post('/', (req, res) => {
+//     res.send('Got a POST request')
+//   })
+
+// app.put('/user', (req, res) => {
+//     res.send('Got a PUT request at /user')
+//   })
+
+// app.delete('/user', (req, res) => {
+//     res.send('Got a DELETE request at /user')
+//   })
 
 app.listen(port, () => {
-    console.log('Server running on localhost:3000')
+    console.log('Server running on localhost:8080')
 })
